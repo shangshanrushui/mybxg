f222da6a8e7f695d1ca365c8e3a801f9da35bd8e
yangshiquan <1364148100@qq.com>
2017-08-30 17:50:08 +0800

    完成登录模块化

--- public/js/common.js
+++ public/js/common.js
@@ -1,4 +1,4 @@
-define(['jquery','cookie'],function($){
+define(['jquery','template','cookie'],function($,template){
 		//NProgress.start();
 		//NProgress.done();
 	//控制主页菜单折叠和展开
@@ -27,14 +27,17 @@ define(['jquery','cookie'],function($){
 		location.href ='/main/login';
 	}
 	if($.cookie('loginInfo')){
+		//获取用户登录信息，并填充页面
 		var loginInfo =JSON.parse($.cookie('loginInfo'));
-		$('.profile img').attr('src',loginInfo.tc_avatar);
-		$('.profile h4').html(loginInfo.tc_name);
+		// console.log(loginInfo);
+		// $('.profile img').attr('src',loginInfo.tc_avatar);
+		// $('.profile h4').html(loginInfo.tc_name);
+		var tpl ='<div class="avatar img-circle"><img src="{{tc_avatar}}"> </div><h4>{{tc_name}}</h4>'
+		var html =template.render(tpl,loginInfo);
+		$('#profileId').html(html);
 	}
-	//获取用户登录信息，并填充页面
 	
-	// console.log(loginInfo);
-	
+
 })
 	
 
--- public/js/config.js
+++ public/js/config.js
@@ -5,6 +5,7 @@ require.config({
 	paths :{
 		jquery : 'jquery/jquery.min',
 		cookie : 'jquery-cookie/jquery.cookie',
+		template : 'artTemplate/template-web',
 		common : '../js/common',
 		login : '../js/login'
 	}
