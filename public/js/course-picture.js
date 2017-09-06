e1d4aa8b5af73c74cf81210c2ec5c7925a7d1f32
yangshiquan <1364148100@qq.com>
2017-09-06 21:06:56 +0800

    完成图片上传功能(可能有bug)

--- public/js/course-picture.js
+++ public/js/course-picture.js
@@ -1,4 +1,4 @@
-define(['jquery','template','util'],function($,template,util){
+define(['jquery','template','util','uploadify'],function($,template,util){
 	util.setMenu('/course/add');
 	var csId = util.qs('cs_id');
 	//展示数据
@@ -10,6 +10,23 @@ define(['jquery','template','util'],function($,template,util){
 		success:function(data){
 			var html =template('pictureTpl',data.result);
 			$('#pictureInfo').html(html);
+
+			//处理课程封面的上传
+			$('#upfile').uploadify({
+				width : 80,
+				height :'auto',
+				buttonText :'上传图片',
+				buttonClass : 'btn btn-warning btn-sm',
+				itemTemplate : '<span></span>',
+				fileObjName :'cs_cover_original',
+				formData:{cs_id:csId},
+				swf:'/public/assets/uploadify/uploadify.swf',
+				uploader:'/api/uploader/cover',
+				onUploadSuccess:function(f,data){
+					var data = JSON.parse(data);
+					$('.preview img').attr('src',data.result.path);
+				}
+			});
 		}
 	})
 })
