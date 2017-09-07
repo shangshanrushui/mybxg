31a22578c7be6864eea3da98f4e4dbe496feb659
yangshiquan <1364148100@qq.com>
2017-09-07 18:58:05 +0800

    完成编辑和添加编辑功能

--- public/js/course-lesson.js
+++ public/js/course-lesson.js
@@ -1,4 +1,4 @@
-define(['jquery','template','util'],function($,template,util){
+define(['jquery','template','util','bootstrap'],function($,template,util,bootstrap){
 	util.setMenu('/course/list');
 	var csId = util.qs('cs_id');
 	$.ajax({
@@ -7,8 +7,41 @@ define(['jquery','template','util'],function($,template,util){
 		data:{cs_id:csId},
 		dataType:'json',
 		success:function(data){
+			//渲染数据
 			var html =template('templateTpl',data.result);
 			$('#templateInfo').html(html);
+
+			//提交表单公共方法
+			// function submitForm(url,ctCsId,ctId){
+			// 	$('#m')
+			// }
+
+			//实现课程添加课时功能
+			$('#addBtn').click(function(){
+				var html =template('modalTpl',{operate:'添加课时'});
+				$('#modalInfo').html(html);
+				$('#chapterModal').modal();
+
+			});
+			$('.editLesson').click(function(){
+				var ctId =$(this).attr('data-ctId');
+				console.log(ctId);
+				$.ajax({
+					type:'get',
+					url:'/api/course/chapter/edit',
+					data:{ct_id:csId},
+					dataType:'json',
+					success:function(data){
+						console.log(data);
+						data.result.operate='编辑课时';
+						var html =template('modalTpl',data.result);
+						$('#modalInfo').html(html);
+						//submitForm('/api/course/chapter/modify',csId,ctId);
+					}
+				});
+				//显示弹窗
+				$('#chapterModal').modal();
+			})
 		}
 	})
 })
